8a651b9c41496f274c94b085e53154a8ab25b8af
VinyasK <158804107+VinyasK@users.noreply.github.com>
2025-02-07 22:06:13 +0530

    productratings.py

--- productratings.py
+++ productratings.py
@@ -1,10 +1,10 @@
 ratings=[5,3,3.5,4,4.5,3,4.5]
 
-unique_ratings=set(ratings)
+unique_ratings=set(ratings) //removes the duplicate values
 max_count=0
 for rating in unique_ratings:
-    count=ratings.count(rating)
-    if count>max_count:
-        max_count=count
+    count=ratings.count(rating) //counts rating value in ratings list
+    if count>max_count: 
+        max_count=count //updates max count with count
         frequent=rating
 print(frequent)        
