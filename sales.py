be190e4c3ab919f8cac23008932c1eca4669cfd9
VinyasK <158804107+VinyasK@users.noreply.github.com>
2025-02-07 22:01:29 +0530

    sales.py

--- sales.py
+++ sales.py
@@ -1,7 +1,7 @@
 sales_list=[2000,3000,5500,7000,6400,10000,9999]
 
-highest_sale=max(sales_list)
+highest_sale=max(sales_list) //gives highest value from list
 
-index=sales_list.index(highest_sale)
+index=sales_list.index(highest_sale) //gives the index value of highest_sale value
 
 print("Day {} has highest sales of {}".format(index,highest_sale))
